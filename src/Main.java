fd27743988bc1bc08863b52bea38a67dd91824b3
kaymakanski <49451700+kaymakanski@users.noreply.github.com>
2023-03-16 13:45:13 +0200

    Implement the logic to get the pair of employees

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,53 +13,121 @@ public class Main {
         String line = "";
         String cvsSplitBy = ",";
 
-        // Map to store the project durations for each group of employees
-        Map<String, Long> projectDurations = new HashMap<>();
+        List<Record> recordList = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-
             while ((line = br.readLine()) != null) {
-
                 String[] data = line.split(cvsSplitBy);
+                Record record = new Record();
 
-                //int employeeId = Integer.parseInt(data[0]);
-                int projectId = Integer.parseInt(data[1]);
-
-                LocalDate dateFrom = parseDate(data[2]);
-                LocalDate dateTo = data.length == 3 ? LocalDate.now() : parseDate(data[3]);
+                if (!(recordList.isEmpty())) {
+                    for (Record e : recordList) {
+                        if (e.getEmployeeID().equals(data[0])) {
+                            record = e;
+                        } else {
+                            record.setEmployeeID(data[0]);
+                            record.setAssignedProject(data[1]);
+                            record.setDateFrom(parseDate(data[2]));
+                            record.setDateTo(data.length == 3 || data[3].equals("NULL") ? LocalDate.now() : parseDate(data[3]));
+                        }
+                    }
+                } else {
+                    record.setEmployeeID(data[0]);
+                    record.setAssignedProject(data[1]);
+                    record.setDateFrom(parseDate(data[2]));
+                    record.setDateTo(data.length == 3 || data[3].equals("NULL") ? LocalDate.now() : parseDate(data[3]));
+                }
+                /*System.out.println("Employee ID is: " + record.getEmployeeID());
+                System.out.println("Project ID is: " + record.getAssignedProject());*/
 
                 // Calculate the duration of the project for this employee
-                long duration = ChronoUnit.DAYS.between(dateFrom, dateTo);
-
-                // Add this employee's duration to the total for this project
-                String projectKey = String.valueOf(projectId);
-                projectDurations.put(projectKey, projectDurations.getOrDefault(projectKey, 0L) + duration);
+                long duration = ChronoUnit.DAYS.between(record.getDateFrom(), record.getDateTo());
+               // System.out.println("Duration is: " + duration);
+                recordList.add(record);
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // Find the group of employees with the longest project duration
-        long maxDuration = 0;
-        String maxDurationKey = "";
-        for (String key : projectDurations.keySet()) {
-            if (projectDurations.get(key) > maxDuration) {
-                maxDuration = projectDurations.get(key);
-                maxDurationKey = key;
+        List<Pair> pairsWhoWorkedTogether = findAllPairsWhoWorkedTogether(recordList);
+       /* for (Pair p : pairsWhoWorkedTogether){
+            System.out.println("Employee " + p.getFirstEmployeeId() + " and Employee " + p.getSecondEmployeeId() + " have worked " + p.getTotalDuration() + " days together!");
+        }*/
+        pairsWhoWorkedTogether.sort(Comparator.comparingLong(Pair::getTotalDuration));
+       /* for (Pair p : pairsWhoWorkedTogether){
+            System.out.println("Employee " + p.getFirstEmployeeId() + " and Employee " + p.getSecondEmployeeId() + " have worked " + p.getTotalDuration() + " days together!");
+        }*/
+        Pair longestPair = pairsWhoWorkedTogether.get(pairsWhoWorkedTogether.size() - 1);
+        System.out.println("Employee " +
+                longestPair.getFirstEmployeeId() + " and Employee " +
+                longestPair.getSecondEmployeeId() + " have worked the longest together for " +
+                longestPair.getTotalDuration() + " days!");
+    }
+
+    private static List<Pair> findAllPairsWhoWorkedTogether(List<Record> allRecords){
+        List<Pair> pairs = new ArrayList<>();
+
+        for (int i = 0; i < allRecords.size() - 1; i++){
+            for (int j = i + 1; j < allRecords.size(); j++){
+                Record firstEmployee = allRecords.get(i);
+                Record secondEmployee = allRecords.get(j);
+
+                if(firstEmployee.getAssignedProject().equals(secondEmployee.getAssignedProject())
+                        && haveWorkedTogether(firstEmployee, secondEmployee)) {
+                    long daysWorkedTogether = calculateDaysWorkedTogether(firstEmployee, secondEmployee);
+
+                    if(daysWorkedTogether > 0){
+                        updatePairs(pairs, firstEmployee, secondEmployee, daysWorkedTogether);
+                    }
+                }
+            }
+        }
+        return pairs;
+    }
+
+    private static void updatePairs(List<Pair> pairs, Record firstEmpl, Record secondEmpl, long daysWorkedTogether){
+        boolean isPairPresent = false;
+
+        for (Pair p : pairs){
+            if (isPairPresent(p, firstEmpl.getEmployeeID(), secondEmpl.getEmployeeID())){
+                p.addMutualDuration(daysWorkedTogether);
+                isPairPresent = true;
             }
         }
 
-        // Find the employees who worked on this project and return their IDs as an array
-        String[] employees = maxDurationKey.split("-");
-        int[] employeeIds = new int[employees.length];
-        for (int i = 0; i < employees.length; i++) {
-            employeeIds[i] = Integer.parseInt(employees[i]);
+        if (!isPairPresent){
+            Pair newPair = new Pair(firstEmpl.getEmployeeID(), secondEmpl.getEmployeeID(), daysWorkedTogether);
+            pairs.add(newPair);
         }
-        for(int i = 0; i < employeeIds.length; i++){
-            System.out.println(employeeIds[i]);
-        }
+    }
 
+    private static long calculateDaysWorkedTogether(Record firstEmpl, Record secondEmpl){
+        LocalDate mutualWorkStartDate =
+                firstEmpl.getDateFrom().isBefore(secondEmpl.getDateFrom()) ?
+                        secondEmpl.getDateFrom() : firstEmpl.getDateFrom();
+
+        LocalDate mutualWorkEndDate =
+                firstEmpl.getDateTo().isBefore(secondEmpl.getDateTo()) ?
+                        firstEmpl.getDateTo() : secondEmpl.getDateTo();
+
+        return ChronoUnit.DAYS.between(mutualWorkStartDate, mutualWorkEndDate);
+    }
+
+    // Check if the current pair is already in the collection
+    private static boolean isPairPresent(Pair pair, String firstEmplId, String secondEmplId) {
+        return ( pair.getFirstEmployeeId().equals(firstEmplId)
+                && pair.getSecondEmployeeId().equals(secondEmplId))
+                || ( pair.getFirstEmployeeId().equals(secondEmplId)
+                && pair.getSecondEmployeeId().equals(firstEmplId));
+    }
+
+    // check if two employees have worked together
+    private static boolean haveWorkedTogether(Record firstEmpl, Record secondEmpl) {
+        //firstStartDate <= secondEndDate and firstEndDate >= secondStartDate
+        return (firstEmpl.getDateFrom().isBefore(secondEmpl.getDateTo())
+                || firstEmpl.getDateFrom().isEqual(secondEmpl.getDateTo()))
+                && (firstEmpl.getDateTo().isAfter(secondEmpl.getDateFrom())
+                || firstEmpl.getDateTo().isEqual(secondEmpl.getDateFrom()));
     }
 
     private static LocalDate parseDate(String dateString) {
@@ -68,7 +135,7 @@ public class Main {
             return LocalDate.now();
         }
 
-        DateTimeFormatter formatter = null;
+        DateTimeFormatter formatter;
         if (dateString.matches("\\d{4}-\\d{2}-\\d{2}")) {
             formatter = DateTimeFormatter.ISO_LOCAL_DATE;
         } else if (dateString.matches("\\d{2}-\\d{2}-\\d{2}")) {
